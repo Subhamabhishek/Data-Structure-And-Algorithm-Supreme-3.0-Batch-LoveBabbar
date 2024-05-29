14827b62edcfe7dcb7431c7d97843616c6b3624c
Subham Abhishek Tripathy <subhamabhishektripathy@gmail.com>
2024-05-29 23:18:23 +0530

    Add files via upload

---
+++ Intro to cpp- week 1/week-1/function.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// void printCounting(){
+//     for(int i=1;i<=5; i++){
+//         cout<<i<<" ";
+//     }cout<<endl;
+
+// }
+
+int sum(int a, int b){
+
+    int totalSum= a+b;
+    return totalSum;
+    
+}
+int main(){
+
+    // printCounting();
+
+    // cout<< "Subham"<<endl;
+
+    // printCounting();
+
+    int ans= sum(5,10);
+    cout<< ans << endl;
+
+
+
+    return 0;
+}
